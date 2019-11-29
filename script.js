38eabf659c478910544bfb2adbfa7f1c7b1beb68
Dioris Diaz <diorid@gmail.com>
2019-11-28 23:35:16 -0500

    moving forward

--- script.js
+++ script.js
@@ -1,5 +1,23 @@
+var generates = {lower: lowerCase, upper: upperCase, number: numberGen, symbols: symbolGen,}
+
+// heres where The link to the charcord chart  that Used http://www.net-comber.com/charset.html
+
 function  lowerCase() {
-    return String.fromCharCode(97);
+    return String.fromCharCode(Math.floor(Math.random() *26) + 97 );
  
  }
- console.log(Math.floor(Math.random() *26) + 97 );
+ console.log(lowerCase());
+
+ function  upperCase() {
+    return String.fromCharCode(Math.floor(Math.random() *26) + 65 );
+ 
+ }
+ console.log(upperCase());
+
+
+ function  symbolGen() {
+    return String.fromCharCode(Math.floor(Math.random() *6) + 58 ); 
+ }
+ console.log(symbolGen());
+
+ 
