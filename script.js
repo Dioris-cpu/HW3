c163297446a626fddc2a9edbac90591d23d18922
Dioris Diaz <diorid@gmail.com>
2019-11-29 04:23:43 -0500

    its comming along pretty good

--- script.js
+++ script.js
@@ -1,4 +1,22 @@
-var generates = {lower: lowerCase, upper: upperCase, number: numberGen, symbols: symbolGen,}
+var formEl = document.getElementById("gform");
+var lengthEl = document.getElementById("length");
+var lowercaseEl = document.getElementById("uppercase");
+var lowercaseEl = document.getElementById("lowercase");
+var numbersEl = document.getElementById("numbers");
+var symbolsEl = document.getElementById("symbols");
+var clipboardEl = document.getElementById("clipboard");
+var generateEl = document.getElementById("generate");
+
+
+generateEl.addEventListener("click",() => {
+    var length = +lengthEl.value; 
+
+    console.log(typeof length);
+ 
+})
+
+
+var generates = {lower: lowerCase, upper: upperCase, number: numberGen, symbols: symbolGen}
 
 // heres where The link to the charcord chart  that Used http://www.net-comber.com/charset.html
 
@@ -13,6 +31,13 @@ function  lowerCase() {
  
  }
  console.log(upperCase());
+
+ function  numberGen() {
+    return String.fromCharCode(Math.floor(Math.random() *10) + 48 );
+ 
+ }
+ console.log(numberGen());
+
 
 
  function  symbolGen() {
