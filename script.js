2de61f20c1927a51ff4db1c1770c439c7a27aaf8
Dioris Diaz <diorid@gmail.com>
2019-12-05 02:45:53 -0500

    like 90% there

--- script.js
+++ script.js
@@ -1,86 +1,54 @@
-var passwordEl = document.getElementById("gpassword");
-var lengthEl = document.getElementById("length");
-var uppercaseEl = document.getElementById("uppercase");
-var lowercaseEl = document.getElementById("lowercase");
-var numbersEl = document.getElementById("numbers");
-var symbolsEl = document.getElementById("symbols");
-var generateEl = document.getElementById("generate");
-
-var generateFunc = {lower: lowerCase, upper: upperCase, number: numberGen, symbols: symbolGen };
+var lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var uppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8"];
+var symbols = ["!", "@", "#", "$", "%", "^", "&", "*"];
+var submitElt = document.getElementById("submit");
+var textDisplayElt = document.getElementById("display")
 
 
-generateEl.addEventListener("click",() => {
-   var length = +lengthEl.value;
-   var checkForLower = lowercaseEl.checked; 
-   var checkForUpper = uppercaseEl.checked;
-   var checkForNumbers = numbersEl.checked;
-   var checkForSymbols = symbolsEl.checked; 
- 
-    
+submitElt.addEventListener("click", function(){
+      var passwordLength = 20;
+      textDisplayElt.value = generatePassword(passwordLength);
+    });
 
+function generatePassword(passwordLength) {
+
+   var password = "";
+
+   for(var i = 0; i < passwordLength; ++i){
+    var length = lowercase.length;
+    var lowerLetters = Math.floor(Math.random() * length);
+    var lower = lowercase[lowerLetters];
+    password = password + lower; 
+   };
+
+   for(var i = 0; i < passwordLength; ++i){
+      var length = uppercase.length;
+      var upperLetters = Math.floor(Math.random() * length);
+      var upper = uppercase[upperLetters];
+      password = password + upper; 
+     };
+     
+     for(var i = 0; i < passwordLength; ++i){
+      var length = numbers.length;
+      var randomNumbers = Math.floor(Math.random() * length);
+      var nums = numbers[randomNumbers];
+      password = password + nums; 
+     };
+     
+     for(var i = 0; i < passwordLength; ++i){
+      var length = symbols.length;
+      var symbolsGen = Math.floor(Math.random() * length);
+      var sym = symbols[symbolsGen];
+      password = password + sym; 
+     } 
+
+  
    
-   passwordEl.innerText = generatePassword(checkForLower, checkForUpper, checkForNumbers, checkForSymbols,length);
-   
+   return password
 
-   console.log(checkForLower, checkForUpper, checkForNumbers, checkForSymbols, length);
-
-});
-
-function generatePassword(lower,upper,number,symbols,length) {
-   
-   var generatedPassword = "";
-
-   var typesCount = lower + upper + number + symbols;
-
-   console.log("typesCount:", typesCount);
-
-   var typesArr = [{lower}, {upper}, {number}, {symbols}].filter (item => Object.values(item)[0]);
-
-   console.log("typesArr:", typesArr);
-
-   if (typesCount === 0) {
-      return '';
-   }
-   for( var i = 0; i < length;i += typesCount) {
-      typesArr.forEach(type => {
-         var funcName = Object.keys(type)[0];
-         console.log( "funcName:", funcName);
-         
-         generatedPassword += generateFunc[funcName] ();
-      });
-        var finalPassword =generatedPassword.slice(0, length);
-        
-        return finalPassword
-
-   }
-   
-
-
-};
-
-// Heres the link to the charcord chart that I used http://www.net-comber.com/charset.html
-
-function  lowerCase() {
-    return String.fromCharCode(Math.floor(Math.random() *26) + 97 ) ;
- 
- }
- console.log(lowerCase());
-
- function  upperCase() {
-    return String.fromCharCode(Math.floor(Math.random() *26) + 65 ) ;
- 
- }
- console.log(upperCase());
-
- function  numberGen() {
-    return String.fromCharCode(Math.floor(Math.random() *10) + 48 ) ;
- 
- }
- console.log(numberGen());
-
-
-
- function  symbolGen() {
-   return String.fromCharCode(Math.floor(Math.random() *6) + 58 ) ; 
 }
-console.log(symbolGen());
+
+var passwordLength = 20;
+var password = generatePassword(passwordLength);
+
